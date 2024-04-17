17c074926fd45b794d86eeecf51bcf29ab20d636
Josu Fabri <jdesimone@frba.utn.edu.ar>
2024-04-17 15:16:38 +0000

    Creado funcion iniciar logger y config, para kernel

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -1,8 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <utils/hello.h>
+#include <utils/utils.h>
 
 int main(int argc, char* argv[]) {
-    decir_hola("Kernel");
+
+    t_log *logger;
+	t_config *config;
+
+    // decir_hola("Kernel");
+    logger = iniciar_logger("kernel");
+    log_info(logger,"Hola soy kernel");
+
     return 0;
 }
---
+++ utils/src/utils/utils.c
@@ -0,0 +1,25 @@
+#include <utils/utils.h>
+
+void decir_hola(char *quien)
+{
+    printf("Hola desde %s!!\n", quien);
+}
+
+t_log *iniciar_logger(char *nombre)
+{
+    // Creando el nombre del archivo
+    char *archivo = malloc(strlen(nombre) + strlen(".log") + 1);
+    strcpy(archivo, nombre);
+    strcat(archivo, ".log");
+
+    t_log *nuevo_logger = log_create(archivo, nombre, true, LOG_LEVEL_INFO);
+
+    return nuevo_logger;
+}
+
+t_config *iniciar_config(char *archivo)
+{
+    t_config *nuevo_config = config_create(archivo);
+
+    return nuevo_config;
+}
